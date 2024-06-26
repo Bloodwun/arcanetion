9d1cc4a1527a9bffa50e9ddddcb6fe1d9a14aeee
abbasDD <g.abbas275@yahoo.com>
2024-06-26 17:25:33 +0500

    chat page issue fixed

--- resources/views/frontend/chats.blade.php
+++ resources/views/frontend/chats.blade.php
@@ -30,15 +30,15 @@
                     <div class="row">
                         <div class="col-12 p-1">
                             {{-- Messages --}}
-                            <div class="card messages" style="height: 440px;">
-                                <div class="card-body">
-                                    <div class="item bg-gray-50 text-right p-2">
+                            <div class="messages" style="height: 440px;">
+                                <div class="">
+                                    <div class="item bg-gray-50 p-2">
                                         <p class="mb-0">My message</p>
-                                        <span class="mb-0 fs-xxs text-right text-muted">11:30 PM</span>
+                                        <span class="mb-0 fs-xxs text-muted">11:30 PM</span>
                                     </div>
-                                    <div class="item bg-gray-50 text-right p-2">
+                                    <div class="item bg-gray-50  p-2">
                                         <p class="mb-0">My message</p>
-                                        <span class="mb-0 fs-xxs text-right text-muted">11:30 PM</span>
+                                        <span class="mb-0 fs-xxs text-muted">11:30 PM</span>
                                     </div>
                                     <div class="item bg-gray-50 p-2">
                                         <p class="mb-0">Your message</p>
@@ -51,38 +51,42 @@
                                 </div>
                                 {{-- Dice --}}
                                 <div class="dice">
-                                    {{-- <img src="{{ asset('images/D6.png') }}" width="50" height="50" alt="Dice"> --}}
+                                    {{-- Input number field --}}
+                                    <div class="position-absolute" style="bottom: 5px; left:50px">
+                                        <input type="text" class="form-control" style="width: 40px;" placeholder="Dic">
+                                    </div>
+                                    {{-- <img src="{{ asset('images/D6.png') }}" width="30" height="30" alt="Dice"> --}}
                                     {{-- Dice Selector --}}
                                     <div class="position-absolute cursor-pointer" style="bottom: 5px;">
                                         <div class="mm-dropdown">
                                             <div class="textfirst">
-                                                <img src="{{ asset('images/D4.png') }}" width="50" height="50"
+                                                <img src="{{ asset('images/D4.png') }}" width="30" height="30"
                                                     class="down" />
                                             </div>
                                             <ul>
                                                 <li class="input-option" data-value="5">
-                                                    <img src="{{ asset('images/D6.png') }}" alt="" width="50"
-                                                        height="50" />
+                                                    <img src="{{ asset('images/D6.png') }}" alt="" width="30"
+                                                        height="30" />
                                                 </li>
                                                 <li class="input-option" data-value="4">
-                                                    <img src="{{ asset('images/D8.png') }}" alt="" width="50"
-                                                        height="50" />
+                                                    <img src="{{ asset('images/D8.png') }}" alt="" width="30"
+                                                        height="30" />
                                                 </li>
                                                 <li class="input-option" data-value="3">
-                                                    <img src="{{ asset('images/D10.png') }}" alt="" width="50"
-                                                        height="50" />
+                                                    <img src="{{ asset('images/D10.png') }}" alt="" width="30"
+                                                        height="30" />
                                                 </li>
                                                 <li class="input-option" data-value="2">
-                                                    <img src="{{ asset('images/D12.png') }}" alt="" width="50"
-                                                        height="50" />
+                                                    <img src="{{ asset('images/D12.png') }}" alt="" width="30"
+                                                        height="30" />
                                                 </li>
                                                 <li class="input-option" data-value="1">
-                                                    <img src="{{ asset('images/D20.png') }}" alt="" width="50"
-                                                        height="50" />
+                                                    <img src="{{ asset('images/D20.png') }}" alt="" width="30"
+                                                        height="30" />
                                                 </li>
                                                 <li class="input-option" data-value="0">
-                                                    <img src="{{ asset('images/D100.png') }}" alt="" width="50"
-                                                        height="50" />
+                                                    <img src="{{ asset('images/D100.png') }}" alt="" width="30"
+                                                        height="30" />
                                                 </li>
                                             </ul>
                                         </div>
@@ -91,9 +95,9 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="card card-transparent" style="height: 50px;">
+                            <div class="card-transparent" style="height: 50px;">
                                 {{-- Input with button and emoji --}}
-                                <div class="card-body d-flex gap-3">
+                                <div class="d-flex gap-3">
                                     <div class="w-100">
                                         {{-- Input group --}}
                                         <div class="input-group">
@@ -131,7 +135,7 @@
             var li = $('div.mm-dropdown > ul > li.input-option')
             var inputoption = $("div.mm-dropdown .option")
             var default_text =
-                '<img src="{{ asset('images/D4.png') }}" width="50" height="50" class="down" />';
+                '<img src="{{ asset('images/D4.png') }}" width="30" height="30" class="down" />';
 
             // Animation
             main.click(function() {
